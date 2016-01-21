6272f6a4be7fe91ed21257af491c66aa5d7a03de
ruben <ruben@Portatil-Acer>
2016-01-21 21:35:31 +0100

    inicio proyecto

--- src/gestionparking/Coche.java
+++ src/gestionparking/Coche.java
@@ -1,7 +1,11 @@
 package gestionparking;
 public class Coche extends Vehiculo {
-
+    private String test = "Soy un coche loco";
     private String tipo;
+
+    public String getTest() {
+        return test;
+    }
 
     public Coche(String tipo, String matricula, String nif_dueño) {
         super(matricula, nif_dueño);
