0f5a3971ffe876c665acdc93c4ba8079bfc6360c
ruben <ruben@Portatil-Acer>
2016-01-21 21:35:25 +0100

    inicio proyecto

--- src/gestionparking/GestionParking.java
+++ src/gestionparking/GestionParking.java
@@ -16,7 +16,21 @@ public class GestionParking {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("OE LOCOO");
+        String prueba=" ";
+        Vehiculo comprobar;
+        Vehiculo v1 = new Moto("V2835GB","23847658Q");
+        Plaza p1 = new Plaza(4,2);
+        p1.setVeh_plaza(v1);
+       comprobar = p1.getVeh_plaza();
+        if(comprobar instanceof Coche){
+        Coche c1 = (Coche) comprobar;
+        prueba = c1.getTest();
+        }else if(comprobar instanceof Moto){
+        Moto m1 = (Moto) comprobar;
+        prueba = m1.getTest();
+        }
+        System.out.println(prueba);
+        
     }
     
 }
