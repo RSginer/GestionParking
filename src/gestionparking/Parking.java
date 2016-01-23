61496040418369c529e3621c80c037e2de275d8a
ruben <ruben@Portatil-Acer>
2016-01-23 22:27:16 +0100

    metodo precio de plaza y constructor de parking

--- src/gestionparking/Parking.java
+++ src/gestionparking/Parking.java
@@ -1,5 +1,8 @@
 package gestionparking;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +14,48 @@ package gestionparking;
  * @author ruben
  */
 public class Parking {
-    
+   Vehiculo cocheJefe = new Coche("L","J9821VLC","23847658Q");
+   public String nombreDeParking;
+   public String direccion;
+   private Map <String,Plaza> listaPlazas = new HashMap<>();
+
+    public Parking(String nombreDeParking, String direccion) {
+        this.nombreDeParking = nombreDeParking;
+        this.direccion = direccion;
+        Plaza p1 = new Plaza(1,1,"C");
+        Plaza p2 = new Plaza(1,2,"L");
+        p2.setVeh_plaza(cocheJefe);
+        Plaza p3 = new Plaza(1,3,"C");
+        Plaza p4 = new Plaza(1,4,"L");
+        Plaza p5 = new Plaza(2,1,"C");
+        Plaza p6 = new Plaza(2,2,"L");
+        Plaza p7 = new Plaza(2,3,"C");
+        Plaza p8 = new Plaza(2,4,"L");
+        listaPlazas.put("" + p1.getNum_sotano() + "" + p1.getNum_plaza(),p1);
+        listaPlazas.put("" + p2.getNum_sotano() + "" + p2.getNum_plaza(),p2);
+        listaPlazas.put("" + p3.getNum_sotano() + "" + p3.getNum_plaza(),p3);
+        listaPlazas.put("" + p4.getNum_sotano() + "" + p4.getNum_plaza(),p4);
+        listaPlazas.put("" + p5.getNum_sotano() + "" + p5.getNum_plaza(),p5);
+        listaPlazas.put("" + p6.getNum_sotano() + "" + p6.getNum_plaza(),p6);
+        listaPlazas.put("" + p7.getNum_sotano() + "" + p7.getNum_plaza(),p7);
+        listaPlazas.put("" + p8.getNum_sotano() + "" + p8.getNum_plaza(),p8);
+    }
+
+    public String getNombreDeParking() {
+        return nombreDeParking;
+    }
+
+    public void setNombreDeParking(String nombreDeParking) {
+        this.nombreDeParking = nombreDeParking;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+   
+   
 }
--- src/gestionparking/Plaza.java
+++ src/gestionparking/Plaza.java
@@ -1,14 +1,16 @@
 package gestionparking;
+
 public class Plaza {
-    
+
     Vehiculo v;
     private int num_plaza;
     private int num_sotano;
     private String tipo_plaza;
 
-    public Plaza(int num_plaza, int num_sotano) {
+    public Plaza(int num_sotano,int num_plaza, String tipo_plaza) {
         this.num_plaza = num_plaza;
         this.num_sotano = num_sotano;
+        this.tipo_plaza = tipo_plaza;
     }
 
     public Vehiculo getVeh_plaza() {
@@ -42,29 +44,30 @@ public class Plaza {
     public void setTipo_veh(String tipo_plaza) {
         this.tipo_plaza = tipo_plaza;
     }
-    
-    public int precio(Vehiculo v){
-        int precio=0;
-    if(v instanceof Moto){
-    precio = 25;
-    }else if(v instanceof Coche){
-        Coche c = (Coche) v;
-        if(c.getTipo().equalsIgnoreCase("C")){
-        precio=40;
-        }else{
-        precio=55;
-        }
-    }
-    return precio;}
-    
-    @Override
-    public String toString(){
-    String texto=("Nº Plaza: " + num_plaza + " Sótano" + num_sotano);
-    if(v==null){
-        texto=texto.concat("\n No hay ningun vehiculo ocupando esta plaza");
-    }else{
-        texto=texto.concat("\n Vehiculo: " + v.getMatricula() + " " + v.getModelo() + " " + v.getColor());
-    }
-    return texto;}
-}
 
+    public int precio(Vehiculo v) {
+        int precio = 0;
+        if (v instanceof Moto) {
+            precio = 25;
+        } else if (v instanceof Coche) {
+            Coche c = (Coche) v;
+            if (c.getTipo().equalsIgnoreCase("C")) {
+                precio = 40;
+            } else {
+                precio = 55;
+            }
+        }
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        String texto = ("Nº Plaza: " + num_plaza + " Sótano" + num_sotano);
+        if (v == null) {
+            texto = texto.concat("\n No hay ningun vehiculo ocupando esta plaza");
+        } else {
+            texto = texto.concat("\n Vehiculo: " + v.getMatricula() + " " + v.getModelo() + " " + v.getColor());
+        }
+        return texto;
+    }
+}
